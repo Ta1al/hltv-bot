647b9ec650966d5e1cccd78cb1c068025bd5f282
Talal <talalahmad257@gmail.com>
2022-04-01 12:34:03 +0500

    Added getMatches command

---
+++ src/commands/get_matches.ts
@@ -0,0 +1,85 @@
+/** Command Data
+ * {
+  "name": "get_matches",
+  "description": "Get all matches from hltv.org/matches page",
+  "options": [
+    {
+      "type": 4,
+      "name": "event_id",
+      "description": "Filter matches by event ID."
+    },
+    {
+      "type": 3,
+      "name": "filter",
+      "description": "Filter matches by pre-set categories. Overrides other filters.",
+      "choices": [
+        {
+          "name": "top_tier",
+          "value": "top_tier"
+        },
+        {
+          "name": "lan_only",
+          "value": "lan_only"
+        }
+      ]
+    }
+  ]
+} */
+
+import HLTV, { MatchFilter, MatchPreview } from "hltv";
+import {
+  Message,
+  MessageComponentInteraction,
+  CommandInteraction,
+  MessageActionRow,
+  MessageSelectMenu
+} from "discord.js";
+import { get, set } from "./../cache";
+
+module.exports = async (interaction: CommandInteraction) => {
+  const i = await interaction.deferReply({ fetchReply: true });
+  const eventId = interaction.options.getInteger("event_id") || undefined;
+  const str = interaction.options.getString("filter") || undefined;
+  let filter: MatchFilter | undefined;
+  if (str) filter = str === "top_tier" ? MatchFilter.TopTier : MatchFilter.LanOnly;
+
+  // cache
+  let matches: any;
+  if (!eventId && !filter) matches = get("matches");
+  if (eventId && !filter) matches = get(`matches_${eventId}`);
+  if (filter) matches = get(`matches_${filter}`);
+  if (!matches)
+    matches = await HLTV.getMatches({ eventId, filter }).then((m) => {
+      m = m.slice(0, 25);
+      if (!eventId && !filter) set("matches", m, 6e4);
+      if (eventId && !filter) set(`matches_${eventId}`, m, 6e4);
+      if (filter) set(`matches_${filter}`, m, 6e4);
+      return m;
+    });
+
+  const row = new MessageActionRow().addComponents(
+    new MessageSelectMenu({
+      custom_id: "matches",
+      placeholder: "Select a match",
+      options: matches.map((m: MatchPreview) => ({
+        label: `${m.team1?.name || "TBD"} vs ${m.team2?.name || "TBD"}`,
+        description: `${m.event?.name || m.title || "N/A"}`,
+        value: `${m.id}`
+      }))
+    })
+  );
+
+  await interaction.editReply({ components: [row] });
+
+  const msg = i as Message;
+  const cfilter = (int: MessageComponentInteraction) => int.message.id == i.id;
+  msg
+    .createMessageComponentCollector({ filter: cfilter, idle: 3e4 })
+    .on("collect", async (int) => {
+      if (!int.isSelectMenu()) return;
+      require("./get_match")(int, null, int.values[0], true);
+    })
+    .on("end", () => {
+      interaction.editReply({ content: "Timed out", components: [] });
+    });
+};
