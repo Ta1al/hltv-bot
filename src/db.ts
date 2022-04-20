7569b930b30fe6a45c39488e1089d64d011d6afc
Talal <alalahmad257@gmail.com>
2022-04-20 14:32:38 +0000

    db

---
+++ src/db.ts
@@ -0,0 +1,20 @@
+import { Schema, model } from "mongoose";
+
+const schema = new Schema({
+  name: String,
+  eol: { type: Number, required: true },
+  value: {},
+})
+
+const db = model("db", schema);
+
+export const get = async (name: string) => {
+  const doc = await db.findOne({ name });
+  return doc ? doc.value : null;
+};
+export const set = async (name: string, eol: number, value: any) => {
+  eol += new Date().getTime();
+  return await db.updateOne({ name }, { $set: { eol, value } }, { upsert: true });
+};
+
+setInterval(() => db.deleteMany({ eol: { $lt: new Date().getTime() } }).then(console.log), 10e3);
