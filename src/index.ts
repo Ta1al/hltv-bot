32d7520e5f436a344a3959581817d5629887710a
Talal <alalahmad257@gmail.com>
2022-04-20 14:32:33 +0000

    Mongodb and sweepers

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,43 @@
 import { Client, Intents } from "discord.js";
-const { TOKEN } = process.env;
+import { connect } from "mongoose";
+const { TOKEN, MONGO_URI } = process.env;
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS],
+  sweepers: { messages: { lifetime: 30e3, interval: 30e3 } },
+});
 
 client.once("ready", () => {
+  if (MONGO_URI) {
+    connect(MONGO_URI);
+    require("./db");
+  }
   console.log("Ready");
 });
 
 client.on("interactionCreate", async interaction => {
-  if (interaction.isCommand()) {
+  if (interaction.isCommand() || interaction.isMessageComponent()) {
     try {
-      return require(`./commands/${interaction.commandName}.js`)(interaction, client);
+      let commandName = interaction.isCommand() ?
+        interaction.commandName :
+        interaction.customId.split(" ")[0],
+        command = require(`./commands/${commandName}.js`);
+
+      return interaction.isCommand() ?
+        command.message(interaction) :
+        command.component(interaction);
     } catch (e) {
       console.error(e);
-      return interaction.reply({ content: "Command not found.", ephemeral: true });
+      return interaction.reply({ content: "An error occured.", ephemeral: true });
     }
   }
 });
 
-client.on("rateLimit", (data) => console.log(`Rate limit exceeded: ${data.path}`, data));
-client.on("error", (e) => console.log("ClientError", e));
-client.on("warn", (e) => console.log("ClientWarning", e));
+client.on("rateLimit", data => console.log(`Rate limit exceeded: ${data.path}`, data));
+client.on("error", e => console.log("ClientError", e));
+client.on("warn", e => console.log("ClientWarning", e));
 
-process.on("uncaughtException", (e) => console.log("UncaughtException", e));
-process.on("unhandledRejection", (e) => console.log("UnhandledRejection", e));
+process.on("uncaughtException", e => console.log("UncaughtException", e));
+process.on("unhandledRejection", e => console.log("UnhandledRejection", e));
 
 client.login(TOKEN);
